342590f80cf531002f4a75b3c0006479522aabba
Jordan Layfield <layfield@ccdc.cam.ac.uk>
2025-10-15 15:55:09 +0100

    64 bit support NO_JIRA

--- Casks/pgutils.rb
+++ Casks/pgutils.rb
@@ -1,13 +1,18 @@
 cask "pgutils" do
   version "2.1.14.1"
-  sha256 "f43c5908e1d6b50723ccf92ec24fcd89a567fdc016198224d9e51bc2c6e39bc7"
-
-  url "https://github.com/Inedo/pgutil/releases/download/v#{version}/pgutil-osx-arm64.zip"
   name "pgutil"
   desc "CLI for performing actions with ProGet"
   homepage "https://inedo.com"
 
-  depends_on arch: :arm64
+  on_arm do
+    sha256 "f43c5908e1d6b50723ccf92ec24fcd89a567fdc016198224d9e51bc2c6e39bc7"
+    url "https://github.com/Inedo/pgutil/releases/download/v#{version}/pgutil-osx-arm64.zip"
+  end
+
+  on_intel do
+    sha256 "6e84db6fc249428635e935cfc0c69232defacfd5a87e664749efc56f51eef14d"
+    url "https://github.com/Inedo/pgutil/releases/download/v#{version}/pgutil-osx-x64.zip"
+  end
 
   binary "pgutil"
 
