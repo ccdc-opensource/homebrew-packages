f7ec63e23af0a27c62e48f59cf3e782adc098980
Jordan Layfield <77847327+layfield-ccdc@users.noreply.github.com>
2025-10-15 12:04:50 +0100

    Update pgutils.rb NO_JIRA

--- Casks/pgutils.rb
+++ Casks/pgutils.rb
@@ -1,5 +1,5 @@
 cask "pgutils" do
-  version "2.1.13.1"
+  version "2.1.14.1"
   sha256 "9a830eaf3cd079dedd262b25e13949281c910d5475ed267f0765cb15b5b0daf0"
 
   url "https://github.com/Inedo/pgutil/releases/download/v#{version}/pgutil-osx-arm64.zip"
