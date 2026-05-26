45eae2352c97e2d2203cf07f3160315116557827
layfield-ccdc <77847327+layfield-ccdc@users.noreply.github.com>
2026-05-26 14:00:23 +0000

    [create-pull-request] automated change

--- Casks/pgutils.rb
+++ Casks/pgutils.rb
@@ -1,16 +1,16 @@
 cask "pgutils" do
-  version "2.2.9.1"
+  version "2.2.10.1"
   name "pgutil"
   desc "CLI for performing actions with ProGet"
   homepage "https://inedo.com"
 
   on_arm do
-    sha256 "2a78377163f47f6874181d7ce12325027368bbaa7c2fc37688523409754afc01"
+    sha256 "57f7e57c39dfde5e8dec96c485c1097dd66b43d24b965629d38ad384f9082117"
     url "https://github.com/Inedo/pgutil/releases/download/v#{version}/pgutil-osx-arm64.zip"
   end
 
   on_intel do
-    sha256 "c1a6228f6d2c7c22c3ef2429c28a783e33f4ed3e18dd666bf7bf5bb4d210b1a8"
+    sha256 "a4f7720ff790b2399f23037e396f106e4b91f47a476be1158ecfe8f1d3e27d9d"
     url "https://github.com/Inedo/pgutil/releases/download/v#{version}/pgutil-osx-x64.zip"
   end
 
